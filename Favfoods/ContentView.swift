4d8a9d9cd468897e4e8c61a97a2d54bf60b8ed5c
Rizky Indiarto <rizky.indiarto@brank.as>
2022-07-02 23:37:57 +0800

    Initial Commit

---
+++ Favfoods/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Favfoods
+//
+//  Created by Rizky Ian Indiarto on 02/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Favfoods/FavfoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavfoodsApp.swift
+//  Favfoods
+//
+//  Created by Rizky Ian Indiarto on 02/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FavfoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
