52c8a15c69f113beb46d639ed43541a0e06c99b1
Rizky Indiarto <rizky.indiarto@brank.as>
2022-07-07 23:14:20 +0800

    finished

--- Favfoods/DetailView.swift
+++ Favfoods/DetailView.swift
@@ -10,21 +10,47 @@ import SwiftUI
 struct DetailView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: false, content: {
-            VStack (spacing: 0){
-                Image("bigGyoza").padding(.top, 30.0).padding(.bottom, 20)
-                HStack(spacing: 0) {
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text("Orange Juice").font(.custom("Poppins-SemiBold", size: 16)).foregroundColor(Color("darkblack"))
-                        Text("Healthy").font(.custom("Poppins-SemiBold", size: 14)).foregroundColor(Color("grey"))
+            Image("bigGyoza").padding(.top, 30.0).padding(.bottom, 20)
+            VStack(alignment: .leading,spacing:0) {
+                HStack(spacing:0) {
+                    VStack(alignment: .leading,spacing:0) {
+                        Text("Gyoza").font(.custom("Poppins-SemiBold", size: 22)).foregroundColor(Color("darkblack")).padding(.top, 6)
+                        Text("Meal").font(.custom("Poppins-SemiBold", size: 16)).foregroundColor(Color("grey")).padding(.top, 6)
                        
                     }
                     Spacer()
-                    HStack {
-                        Text("4.5").font(.custom("Poppins-SemiBold", size: 16)).foregroundColor(Color("darkblack"))
+                    HStack(spacing:0) {
+                        Text("4.5").font(.custom("Poppins-SemiBold", size: 16)).foregroundColor(Color("darkblack")).padding(.trailing, 4.0)
                         Image("star")
                     }
                 }
-                Text("Makanan asal Bandung dengan tekstur yang lembut sehingga ketika dimakan rasanya tidak sakit tenggorokan.").font(.custom("Poppins-Regular", size: 16)).foregroundColor(Color("darkblack"))
+                Text("Makanan asal Bandung dengan tekstur yang lembut sehingga ketika dimakan rasanya tidak sakit tenggorokan.").font(.custom("Poppins-Regular", size: 16)).foregroundColor(Color("darkblack")).padding(.top, 16).padding(.bottom, 20)
+                VStack(alignment: .leading, spacing: 0.0 ) {
+                    Text("Bundle").font(.custom("Poppins-Bold", size: 22)).foregroundColor(Color("darkblack")).padding(.bottom, 18)
+                    HStack(spacing: 0.0) {
+                        Image("orange").padding(.trailing, 16)
+                        Image("orange").padding(.trailing, 16)
+                        Image("orange").padding(.trailing, 16)
+                    }
+                }.padding(.bottom, 18)
+                HStack {
+                    VStack(alignment: .leading, spacing: 0.0 ) {
+                        Text("$800").font(.custom("Poppins-SemiBold", size: 22)).foregroundColor(Color("darkblack")).padding(.bottom, 6)
+                        Text("/porsi").font(.custom("Poppins-SemiBold", size: 14 )).foregroundColor(Color("grey")).padding(.bottom, 18)
+                        HStack(spacing: 0.0) {
+                         
+                        }
+                    }
+                    Spacer()
+                    Button(action: {
+                    },
+                           label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 50).frame(width: 190, height: 50).foregroundColor(Color("oranges"))
+                            Text("Order").font(.custom("Poppins-SemiBold", size: 16)).foregroundColor(Color("ash"))
+                        }
+                    })
+                }
             }
         })
     }
--- Favfoods/GetStartedView.swift
+++ Favfoods/GetStartedView.swift
@@ -8,24 +8,35 @@
 import SwiftUI
 
 struct GetStartedView: View {
+    @State private var showHomePage: Bool = false
     var body: some View {
-        VStack {
-            Image("splash").padding(.bottom, 50.0)
-            HStack {
-                VStack(alignment: .leading) {
-                        Text("Pentol Store").font(.custom("Poppins-Regular", size: 16)).foregroundColor(Color("grey"))
-                        Text("Order Your\nFavourite Foods")
-                            .font(.custom("Poppins-Bold", size: 36))
-                            .foregroundColor(Color("darkblack"))
-                        Button(action: {
-                        },
-                               label: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 50).frame(width: 328, height: 50).foregroundColor(Color("oranges"))
-                                Text("Explore Now").font(.custom("Poppins-SemiBold", size: 16)).foregroundColor(Color("ash"))
-                            }
-                        })
-                    
+        return Group {
+            if showHomePage {
+                HomeView()
+            } else {
+                
+            }
+        }
+        NavigationView {
+            VStack {
+                Image("splash").padding(.bottom, 50.0)
+                HStack {
+                    VStack(alignment: .leading) {
+                            Text("Pentol Store").font(.custom("Poppins-Regular", size: 16)).foregroundColor(Color("grey"))
+                            Text("Order Your\nFavourite Foods")
+                                .font(.custom("Poppins-Bold", size: 36))
+                                .foregroundColor(Color("darkblack"))
+                        
+                            Button(action: {
+                            },
+                                   label: {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 50).frame(width: 328, height: 50).foregroundColor(Color("oranges"))
+                                    Text("Explore Now").font(.custom("Poppins-SemiBold", size: 16)).foregroundColor(Color("ash"))
+                                }
+                            })
+                        
+                    }
                 }
             }
         }
